830f25935e49d00cb41cd1174e895462d501278b
Lucas Badiale <lcbadial@gmail.com>
2015-08-04 23:58:21 -0300

    yo angular
    Gerado código automaticamente utilizando yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miniCommerceApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the miniCommerceApp
+ */
+angular.module('miniCommerceApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name miniCommerceApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the miniCommerceApp
+ */
+angular.module('miniCommerceApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
